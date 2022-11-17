42d8c628147bfeb38202fb083c587b26e4d4a7eb
russgo1 <russellg580@gmail.com>
2022-11-17 14:50:22 -0500

    7.3.5

--- schema_pg.sql
+++ schema_pg.sql
@@ -151,4 +151,51 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
-SELECT * FROM dept_retirees;
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Joining more than two tables (stupid becasue of extra steps)
+SELECT e.emp_no, 
+	e.first_name, 
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s -- first join
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp AS de -- second join
+ON (e.emp_no = de.emp_no)
+-- Must use aliases in filters, too
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- Managers' emp nums, names, start/end dates, dept num and name
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+		
+-- Current retirees number, name, and dept
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+	
