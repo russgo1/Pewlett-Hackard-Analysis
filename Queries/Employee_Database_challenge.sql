f6556674000d13361f28801fb9287311566b73c6
russgo1 <114126935+russgo1@users.noreply.github.com>
2022-11-23 17:47:39 -0500

    Module 7 Challenge

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,51 @@
+-- Deliverable 1
+-- Retirement Titles table
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Unique Titles table
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, title DESC;
+
+-- Retiring Titles table
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY "count" DESC;
+
+-- Deliverable 2
+-- Mentorship Eligibility Table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e 
+	JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no, title;
